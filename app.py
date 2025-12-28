bec752aad1b1262c508ad76237f0144ef7d459be
chahatyadav336 <chahatyadav369.56@gmail.com>
2025-12-28 12:03:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
-from sklearn.preprocessing
-import StandardScaler
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
@@ -10,15 +9,11 @@ st.title('🏠House Price prediction using ML')
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
 df = pd.read_csv('house_data.csv')
-x = df.iloc[:,:-3]
+X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
-
-
 
 final_X = X
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
 
-st.sidebar.title('Select House features:')
-
-
+st.sidebar.title('Select House Features: ')
